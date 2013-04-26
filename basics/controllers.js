c99839d666fb7733866cc8e65a31eaa5d093624b
Christian Ramon <rchristian.ramon@gmail.com>
2013-04-26 12:30:09 -0500

    first commit

---
+++ basics/controllers.js
@@ -0,0 +1,6 @@
+function ProductController($scope) {
+    $scope.products = products=[
+        {product_name: 'IPhone 1', moq: 33},
+        {product_name: 'IPhone 2', moq: 2},
+        {product_name: 'IPhone 3', moq: 7}];
+}
